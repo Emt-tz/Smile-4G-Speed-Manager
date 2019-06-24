7e4a9b8f325c65dc9146564571bbbfd26bd9e795
Emmanuel Mtera <52101851+Emt-tz@users.noreply.github.com>
2019-06-24 09:35:47 +0300

    Update 4g.py

--- 4g.py
+++ 4g.py
@@ -4,9 +4,7 @@ import time
 import sys
 
 class Smile4g():
-
 	def login():
-		
 		#choice = int(input("Enter Speed(1, 2, 5, 21): "))
 		global choice 
 		global browser
@@ -23,10 +21,8 @@ class Smile4g():
 		password.send_keys("password")
 
 		login_attempt = browser.find_element_by_xpath('//*[@id="loginform"]/input[4]').click(); time.sleep(2);
-
 	def confirm():
 		return browser.find_element_by_xpath('//*[@id="content"]/div[4]/div[2]/div/div[2]/div[2]/div/div/form/div[6]/div/input[6]').click()
-
 	def ChangeSpeed():
 		manage = browser.find_element_by_xpath('//*[@id="content"]/div[4]/div[2]/div/div[2]/div[1]/div[2]/table[2]/tbody/tr[2]/td[4]/form/input').click(); time.sleep(0.5);
 		if choice == 1:
@@ -39,14 +35,12 @@ class Smile4g():
 			return browser.find_element_by_xpath('//*[@id="category_speed21"]').click()
 		else:
 			return 0
-		
 	def main():
-
-		login()
+		Smile4g.login()
 		time.sleep(0.5)
-		ChangeSpeed()
+		Smile4g.ChangeSpeed()
 		time.sleep(0.5)
-		confirm()
+		Smile4g.confirm()
 		browser.close()
 		sys.exit()
 		
